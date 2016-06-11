e1089b95db262ef699393fefc8bf6251451a67cb
Philihp Busby <philihp@gmail.com>
2016-06-11 13:39:32 -0700

    Adds authenticable module

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -13,7 +13,6 @@ class Api::V1::SessionsController < ApplicationController
       render json: { errors: "Invalid email or password" }, status: 422
     end
   end
-
   def destroy
     user = User.find_by(auth_token: params[:id])
     user.generate_authentication_token!
