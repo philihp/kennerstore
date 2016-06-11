c90e768600daa198447d873a14fd5d468d589fe2
Philihp Busby <philihp@gmail.com>
2016-06-11 12:51:25 -0700

    Session signin action

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   namespace :api, defaults: { format: :json }, path: '/' do
-    scope module: :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
       resources :users, :only => [:show, :create, :update, :destroy]
+      resources :sessions, :only => [:create, :destroy]
     end
   end
 
