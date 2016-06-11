c5553b5bcb40edee057aaaee55ad15065cf7837e
Philihp Busby <philihp@gmail.com>
2016-06-11 04:17:51 -0700

    Add user update

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1 do
-      resources :users, :only => [:show, :create]
+      resources :users, :only => [:show, :create, :update]
     end
   end
 
