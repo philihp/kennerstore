bd5df092471f683c4739ec4317423ff6339dbde4
Philihp Busby <philihp@gmail.com>
2016-06-11 04:18:39 -0700

    Add user destroy

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1 do
-      resources :users, :only => [:show, :create, :update]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 
